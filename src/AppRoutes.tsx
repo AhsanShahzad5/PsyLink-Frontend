4bc6b66f5b0232a14bf72d5ebcc26516f82201c7
Ahsan Shahzad <112505556+AhsanShahzad5@users.noreply.github.com>
2024-11-15 20:44:02 +0500

    added css font and some stuff

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
-import LoginPage from './pages/LoginPage';
+import LoginPage from './pages/Doctor/LoginPage';
 import DetailForm from './pages/Patient/DetailForm';
 import ChooseSpecialist from './pages/Patient/ChooseSpecialist';
 import HomePage from './pages/Patient/HomePage';
@@ -11,15 +11,18 @@ const AppRoutes = () => {
 
     return (
         <Routes>
+            {/* general routes  */}
             <Route path='/' element={<App/> } />
-            <Route path='/login' element={<LoginPage/> } />
-            <Route path='/doctor/home' element={<LoginPage/> } />
             <Route path='*' element={<Navigate to='/' />} />
+            <Route path='/login' element={<LoginPage/> } />
+            {/* patient routes  */}     
             <Route path='/patient/detailForm' element={<DetailForm/>} />
             <Route path='/patient/chooseSpecialist' element={<ChooseSpecialist/>} />
             <Route path='/patient/Home' element={<HomePage/>} />
             <Route path='/patient/Notes' element={<NotesPage/>} />
             <Route path='/patient/history' element={<Prescription/>} />
+            {/* doctor routes  */}
+            <Route path='/doctor/home' element={<LoginPage/> } />
         </Routes>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			primary: '#02968A',
-  			secondary: '#D3EDEB',
-  			base: '#FFFFFF'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	darkMode: ["class"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#02968A',
+				secondary: '#D3EDEB',
+				base: '#FFFFFF'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			fontFamily: {
+				outfit: ['Outfit', 'sans-serif'], // Add Outfit font
+			},
+		}
+	},
+	plugins: [require("tailwindcss-animate")],
 }
 
