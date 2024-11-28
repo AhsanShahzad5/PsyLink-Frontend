29e011ad5ab93e8eb6c3747676b9f7f9773e137a
Ahsan Shahzad <112505556+AhsanShahzad5@users.noreply.github.com>
2024-11-28 22:44:16 +0500

    ...s

---
+++ src/Components/doctor/DoctorAboutSection.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const DoctorAboutSection = () => {
+  return (
+    <div className='bg-white rounded-lg p-5 my-2'>DoctorIntro</div>
+  )
+}
+
+export default DoctorAboutSection
---
+++ src/Components/doctor/DoctorCalender.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const DoctorCalender = () => {
+  return (
+    <div className='bg-white rounded-lg p-5 my-2'>DoctorIntro</div>
+  )
+}
+
+export default DoctorCalender
